4babcbcf1767831856947f3e5fecf56208fb1b9e
Lucas Viana <oviana@outlook.com>
2022-08-10 13:48:21 -0300

    Update push: delete exports in compiled files

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,14 +2,25 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
+# setup .clasp.json
 source "scripts/setup-clasp.sh"
 
+# build and copy config to build dir
 mkdir -p .build
 npm run build
 cp "appsscript.json" ".claspignore" ".clasp.json" ".build"
 
+# remove export statements from transpiled files
+for file in .build/**/*.js; do
+  sed -r -i "/^export\s.+/d" "$file" &> /dev/null
+done
+for file in .build/*.js; do
+  sed -r -i "/^export\s.+/d" "$file" &> /dev/null
+done
+
+# actually push to GAS (hide output in github because of sensitive data)
 if [ "$ENV" == "GITHUB" ]; then
-  exit $(clasp push &> /dev/null)
+ clasp push --force &> /dev/null
 else
   clasp push --force
 fi
