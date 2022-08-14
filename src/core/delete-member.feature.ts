b30e3258086b962642d225241a36f58e3cb5ad20
Lucas Viana <oviana@outlook.com>
2022-08-13 22:56:41 -0300

    Add confirmation prompt before deleting members

--- src/core/delete-member.feature.ts
+++ src/core/delete-member.feature.ts
@@ -1,13 +1,31 @@
-import { NamedRange, ss } from '@utils/constants';
-import { getTargetNusp, manageMemberSyncedDataSheets } from '@utils/functions';
+import { NamedRange, ss, ui } from '@utils/constants';
+import { getMemberData, getTargetNusp, manageMemberSyncedDataSheets } from '@utils/functions';
 
 /** Delete the member searched in the dashboard from all synced data sheets. */
 export const deleteTargetMember = () => {
   const nusp = getTargetNusp();
 
-  if (nusp) {
-    manageMemberSyncedDataSheets(nusp, (cell, sheet) => sheet.deleteRow(cell.getRow()));
-    ss.getRangeByName(NamedRange.SearchTarget).clearContent();
-    ss.toast('Membro excluído.', 'Sucesso!');
+  if (!nusp) {
+    return;
+  }
+
+  const member = getMemberData(nusp);
+  const response = ui.prompt(
+    'Excluir Membro',
+    `Você tem certeza que deseja excluir ${member.name} (de nº USP "${member.nUsp}")? Essa ação não pode ser desfeita.`,
+    ui.ButtonSet.YES_NO,
+  );
+
+  switch (response.getSelectedButton()) {
+    case ui.Button.YES: {
+      manageMemberSyncedDataSheets(nusp, (cell, sheet) => sheet.deleteRow(cell.getRow()));
+      ss.getRangeByName(NamedRange.SearchTarget).clearContent();
+      ss.toast('Membro excluído.', 'Sucesso!');
+      break;
+    }
+
+    default: {
+      ss.toast('Exclusão de membro cancelada.', 'Operação abortada!');
+    }
   }
 };
