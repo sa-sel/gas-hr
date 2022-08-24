bd211c8b1a99adc6426d27dba2ed12426a11f1d4
Lucas Viana <oviana@outlook.com>
2022-08-23 23:04:21 -0300

    Split taskbar menu into multiple ones

--- src/core/on-open.feature.ts
+++ src/core/on-open.feature.ts
@@ -7,16 +7,17 @@ import { trackMeetingAttendance } from './meeting-attendance.feature';
 export const onOpen = () => {
   ss.getRangeByName(NamedRange.SearchTarget).clearContent();
 
-  const targetMemberOperations = ui
-    .createMenu('Editar Membro Buscado (na Home)')
-    .addItem('Editar Nome', editTargetMemberName.name)
-    .addItem('Editar Apelido', editTargetMemberNickname.name)
-    .addItem('Editar nº USP', editTargetMemberNusp.name);
-
-  ui.createMenu('Magias')
+  ui.createMenu('[Membros]')
     .addItem('Salvar Novos Membros', saveNewMembers.name)
-    .addItem('Marcar Frequência em RG', trackMeetingAttendance.name)
     .addSeparator()
-    .addSubMenu(targetMemberOperations)
+    .addSubMenu(
+      ui
+        .createMenu('Editar Membro Buscado (na Home)')
+        .addItem('Editar Nome', editTargetMemberName.name)
+        .addItem('Editar Apelido', editTargetMemberNickname.name)
+        .addItem('Editar nº USP', editTargetMemberNusp.name),
+    )
     .addToUi();
+
+  ui.createMenu('[RGs]').addItem('Marcar Frequência em RG', trackMeetingAttendance.name).addToUi();
 };
